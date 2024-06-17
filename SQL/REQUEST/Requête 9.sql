65cf8d507a6a489a2bca18da43f642b4fc66d148
Le Cam Yannick <yannick.lecam1@gmail.com>
2024-06-17 16:45:24 +0200

    Requete 9

---
+++ SQL/REQUEST/Requête 9.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT recipe.name,count(price*quantify.quantity) 
+FROM INGREDIENT
+INNER JOIN QUANTIFY
+ON quantify.id_ingredient = ingredient.id_ingredent
+INNER JOIN RECIPE
+ON recipe.id_recipe = quantify.id_recipe
+WHERE recipe.id_recipe = 5
+GROUP BY quantify.id_recipe;
