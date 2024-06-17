b7608e3a0af3772f04302544553ffa40d9ce7de7
Le Cam Yannick <yannick.lecam1@gmail.com>
2024-06-17 16:54:33 +0200

    rename les requetes comme il faut

--- SQL/BDD_STRUCT.sql
+++ SQL/BDD_STRUCT.sql
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS `ingredient` (
   `price` decimal(15,2) unsigned NOT NULL DEFAULT '0.00',
   `unitMeasure` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_ingredent`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table recipe_demo.ingredient : ~19 rows (environ)
+-- Listage des données de la table recipe_demo.ingredient : ~25 rows (environ)
 INSERT IGNORE INTO `ingredient` (`id_ingredent`, `name`, `price`, `unitMeasure`) VALUES
 	(1, 'Oeuf', 0.53, 'unité'),
 	(2, 'Lait', 1.23, 'litre'),
@@ -48,7 +48,13 @@ INSERT IGNORE INTO `ingredient` (`id_ingredent`, `name`, `price`, `unitMeasure`)
 	(16, 'Frommage Feta', 4.23, 'gramme'),
 	(17, 'Huile d\'olive', 2.60, 'cL'),
 	(18, 'Vinaigre balsamique', 4.23, 'cL'),
-	(19, 'Poivre', 0.23, 'gramme');
+	(19, 'Poivre', 0.23, 'gramme'),
+	(20, 'Spaghetti', 2.50, 'gramme'),
+	(21, 'Parmesan', 5.00, 'gramme'),
+	(22, 'Pancetta', 4.00, 'gramme'),
+	(23, 'Spaghetti', 2.50, 'gramme'),
+	(24, 'Parmesan', 5.00, 'gramme'),
+	(25, 'Pancetta', 4.00, 'gramme');
 
 -- Listage de la structure de table recipe_demo. quantify
 CREATE TABLE IF NOT EXISTS `quantify` (
@@ -58,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `quantify` (
   KEY `FK_ingredient` (`id_ingredient`),
   KEY `FK_recipe` (`id_recipe`),
   CONSTRAINT `FK_ingredient` FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id_ingredent`),
-  CONSTRAINT `FK_recipe` FOREIGN KEY (`id_recipe`) REFERENCES `recipe` (`id_recipe`)
+  CONSTRAINT `FK_recipe` FOREIGN KEY (`id_recipe`) REFERENCES `recipe` (`id_recipe`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Listage des données de la table recipe_demo.quantify : ~19 rows (environ)
+-- Listage des données de la table recipe_demo.quantify : ~0 rows (environ)
 INSERT IGNORE INTO `quantify` (`quantity`, `id_ingredient`, `id_recipe`) VALUES
 	(200, 3, 1),
 	(4, 1, 1),
@@ -70,18 +76,18 @@ INSERT IGNORE INTO `quantify` (`quantity`, `id_ingredient`, `id_recipe`) VALUES
 	(150, 4, 1),
 	(1, 5, 1),
 	(12, 6, 1),
-	(200, 9, 2),
-	(1, 10, 2),
-	(1, 11, 2),
-	(2, 12, 2),
-	(150, 13, 2),
-	(100, 14, 2),
-	(100, 15, 2),
-	(100, 16, 2),
-	(6, 17, 2),
-	(4, 18, 2),
-	(6, 6, 2),
-	(6, 19, 2);
+	(500, 20, 3),
+	(3, 1, 3),
+	(100, 21, 3),
+	(150, 22, 3),
+	(5, 6, 3),
+	(2, 19, 3),
+	(500, 20, 3),
+	(3, 1, 3),
+	(100, 21, 3),
+	(150, 22, 3),
+	(5, 6, 3),
+	(2, 19, 3);
 
 -- Listage de la structure de table recipe_demo. recipe
 CREATE TABLE IF NOT EXISTS `recipe` (
@@ -93,12 +99,12 @@ CREATE TABLE IF NOT EXISTS `recipe` (
   PRIMARY KEY (`id_recipe`),
   KEY `Index 2` (`id_type`),
   CONSTRAINT `FK_recipe_type` FOREIGN KEY (`id_type`) REFERENCES `type` (`id_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Listage des données de la table recipe_demo.recipe : ~2 rows (environ)
 INSERT IGNORE INTO `recipe` (`id_recipe`, `name`, `instruction`, `timeCook`, `id_type`) VALUES
 	(1, 'gateau au chocolat', 'Instructions\r\nPréparation du four et des ingrédients :\r\n\r\nPréchauffez votre four à 180°C (350°F).\r\nBeurrez et farinez un moule à gâteau (environ 22 cm de diamètre) ou chemisez-le de papier sulfurisé.\r\nFaire fondre le chocolat et le beurre :\r\n\r\nCassez le chocolat en morceaux et faites-le fondre au bain-marie ou au micro-ondes avec le beurre, en remuant régulièrement jusqu\'à obtention d\'un mélange lisse.\r\nMélanger les œufs et le sucre :\r\n\r\nDans un grand bol, battez les œufs avec le sucre jusqu\'à ce que le mélange blanchisse et devienne mousseux.\r\nAjouter le chocolat fondu :\r\n\r\nIncorporez le mélange de chocolat et de beurre fondu au mélange œufs-sucre. Mélangez bien.\r\nAjouter les ingrédients secs :\r\n\r\nTamisez la farine et la levure chimique au-dessus du bol, puis ajoutez une pincée de sel. Mélangez délicatement jusqu\'à obtention d\'une pâte homogène.\r\nAjouter l\'extrait de vanille :\r\n\r\nSi vous le souhaitez, ajoutez l\'extrait de vanille à la pâte et mélangez.\r\nCuisson :\r\n\r\nVersez la pâte dans le moule préparé.\r\nEnfournez et faites cuire pendant environ 25 à 30 minutes. Pour vérifier la cuisson, insérez un cure-dent au centre du gâteau : il doit en ressortir propre ou avec quelques miettes.\r\nRefroidissement :\r\n\r\nLaissez le gâteau refroidir dans le moule pendant quelques minutes avant de le démouler sur une grille pour qu\'il refroidisse complètement.\r\nDégustation :\r\n\r\nVous pouvez saupoudrer le gâteau de sucre glace ou le napper d\'une ganache au chocolat pour une touche supplémentaire.\r\nBon appétit !\r\n', 55, 1),
-	(2, 'Salade de riz', 'Instructions\r\nCuire le riz :\r\n\r\nFaites cuire le riz selon les instructions du paquet. Égouttez-le et laissez-le refroidir complètement.\r\nPréparer les légumes :\r\n\r\nLavez et coupez les poivrons en petits dés.\r\nÉpluchez et coupez le concombre en petits dés.\r\nLavez et coupez les tomates en petits dés, en retirant les graines si vous le souhaitez.\r\nÉgouttez le maïs.\r\nFaites cuire les petits pois surgelés dans de l\'eau bouillante pendant environ 2-3 minutes, puis égouttez-les et laissez-les refroidir.\r\nMélanger les ingrédients :\r\n\r\nDans un grand saladier, mélangez le riz refroidi, les poivrons, le concombre, les tomates, le maïs et les petits pois.\r\nAjoutez le jambon et le fromage feta ou de chèvre émietté, si vous en utilisez.\r\nAssaisonner la salade :\r\n\r\nDans un petit bol, mélangez l\'huile d\'olive et le vinaigre balsamique ou le jus de citron. Assaisonnez avec du sel et du poivre selon votre goût.\r\nVersez cette vinaigrette sur la salade et mélangez bien pour enrober tous les ingrédients.\r\nGarniture et service :\r\n\r\nGarnissez la salade de feuilles de basilic ou de persil frais.\r\nServez immédiatement ou réfrigérez pendant une heure pour permettre aux saveurs de se mélanger.\r\nAstuces\r\nVous pouvez ajouter d\'autres ingrédients selon vos goûts, comme des olives noires, des carottes râpées, des oignons rouges finement tranchés, ou des herbes fraîches comme de la menthe ou de la coriandre.\r\nPour une version végétarienne, omettez le jambon.', 30, 2);
+	(3, 'Pâtes Carbonara', '1. Faites cuire les pâtes dans de l\'eau salée selon les instructions sur l\'emballage.\n2. Dans un bol séparé, battez les œufs et mélangez-les avec le parmesan râpé.\n3. Faites frire la pancetta dans une poêle jusqu\'à ce qu\'elle soit croustillante.\n4. Égouttez les pâtes et remettez-les dans la casserole. Retirez du feu.\n5. Mélangez rapidement la pancetta, puis le mélange d\'œufs et de fromage. Remuez vigoureusement pour créer une sauce crémeuse.\n6. Assaisonnez avec du sel et du poivre selon votre goût, et servez immédiatement.', 20, 2);
 
 -- Listage de la structure de table recipe_demo. type
 CREATE TABLE IF NOT EXISTS `type` (
@@ -107,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `type` (
   PRIMARY KEY (`id_type`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table recipe_demo.type : ~4 rows (environ)
+-- Listage des données de la table recipe_demo.type : ~0 rows (environ)
 INSERT IGNORE INTO `type` (`id_type`, `typeMeal`) VALUES
 	(1, 'dessert'),
 	(2, 'plat'),
--- SQL/REQUEST/Requête 8.sql
+++ SQL/REQUEST/Requête 8.sql
@@ -4,4 +4,5 @@ INNER JOIN QUANTIFY
 ON quantify.id_ingredient = ingredient.id_ingredent
 INNER JOIN RECIPE
 ON recipe.id_recipe = quantify.id_recipe
+WHERE recipe.id_recipe = 5
 GROUP BY quantify.id_recipe;
